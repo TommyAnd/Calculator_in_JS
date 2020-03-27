65f4b4dc2816dbb8873f49febecf632215d974b5
Enrico <56918333+EnryBarto@users.noreply.github.com>
2020-03-27 12:22:26 +0100

    Added radQ

--- js.js
+++ js.js
@@ -1,33 +1,45 @@
 function Calculator(){
 
 	if (  sessionStorage.getItem("Already opened?") != "Yes" ) {
-		window.alert("ISTRUZIONI PER L'USO DELLA CALCOLATRICE:\n\n• Utilizzare il punto in sostituzione alla virgola (10,5 → 10.5);\n\n• Quando richiesto, inserire il primo numero;\n\n• Quando richiesto, inserire l'operatore:\n\n+ → Addizione\n\n- → Sottrazione\n\n* → Moltiplicazione\n\n/ → Divisione\n\n^ → Potenza\n\n• Quando richiesto, inserire il secondo numero;\n\nN.B.: In caso di potenza il primo numero rappresenta la base e il secondo numero rappresenta l'esponente.");
+		window.alert("ISTRUZIONI PER L'USO DELLA CALCOLATRICE:\n\n• Utilizzare il punto in sostituzione alla virgola (10,5 → 10.5);\n\n• Quando richiesto, inserire il primo numero;\n\n• Quando richiesto, inserire l'operatore:\n\n+ → Addizione\n\n- → Sottrazione\n\n* → Moltiplicazione\n\n/ → Divisione\n\n^ → Potenza;\n\nradQ → Radice quadrata;\n\n• Quando richiesto, inserire il secondo numero;\n\nN.B.: In caso di potenza il primo numero rappresenta la base e il secondo numero rappresenta l'esponente.");
 	}
 
 	sessionStorage.setItem("Already opened?", "Yes");
 
 	var x = prompt("Inserire il primo numero")*1;
 	var op = prompt("Inserire l'operatore");
-	var y = prompt("Inserire il secondo numero")*1;
-
+	
 	if ( op == "+") {
+		var y = prompt("Inserire il secondo numero")*1;
 	    var result = x + y;
 	    window.alert("Il risultato è: "+result);
 	}
 	else if ( op == "-") {
+		var y = prompt("Inserire il secondo numero")*1;
 	    var result = x - y;
 	    window.alert("Il risultato è: "+result);
 	}
 	else if ( op == "*") {
+		var y = prompt("Inserire il secondo numero")*1;
 	    var result = x * y;
 	    window.alert("Il risultato è: "+result);
 	}
 	else if ( op == "^") {
+		var y = prompt("Inserire il secondo numero")*1;
 		var result = 1*1;
 	    for (step = 0; step < y; step++) {
 	    	var result = result * x;
 	    }
 	    window.alert("Il risultato è: "+result);
+	}
+	else if ( op == "radQ") {
+		if ( x >= 0) {
+			var result = Math.sqrt(x);
+	    	window.alert("Il risultato è: "+result);
+		}
+		else{
+			window.alert("La radice quadrata di un numero negativo non esiste");
+		}
 	}
 	else if ( op == "/") {
 		if ( y != 0) {
@@ -47,6 +59,6 @@ function Calculator(){
 	else {
 		window.alert("Operatore non riconosciuto");
 	}
-	
+
 	window.location.reload(true);
 }
